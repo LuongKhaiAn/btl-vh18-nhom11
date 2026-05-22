ae78cc6fa4297551ac2f7cf981f0e61bd3b89355
zanan-cell <dv506685@gmail.com>
2026-05-22 10:35:05 +0700

    văn

--- src/pages/home-page/home-menu.jsx
+++ src/pages/home-page/home-menu.jsx
@@ -1,7 +1,6 @@
 import Footer from "../../components/common/Footer";
 import Navbar from "../../components/common/Navbar";
 import HeroSection from "../../components/layout/HeroSection";
-import HerSection from "../../components/layout/HerSection";
 import ServiceSection from "../../components/layout/ServiceSection";
 
 // Gom cac section cua trang home theo dung cau truc src/pages/home-page.
@@ -12,7 +11,6 @@ const HomeMenu = () => {
       <main>
         <HeroSection />
         <ServiceSection />
-        <HerSection />
       </main>
       <Footer />
     </>
